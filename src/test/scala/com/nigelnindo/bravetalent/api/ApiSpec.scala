a1d0b88ce51f767091552e88834916fa864b23be
Nigel Nindo <nigelnindo@gmail.com>
2017-08-17 18:17:18 +0300

    More Akka HTTP test cases

--- src/test/scala/com/nigelnindo/bravetalent/api/ApiSpec.scala
+++ src/test/scala/com/nigelnindo/bravetalent/api/ApiSpec.scala
@@ -1,5 +1,6 @@
 package com.nigelnindo.bravetalent.api
 
+import akka.http.scaladsl.server.MissingQueryParamRejection
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.scalatest.{Matchers, FlatSpec}
 import akka.http.scaladsl.model.StatusCodes._
@@ -19,16 +20,36 @@ class ApiSpec extends FlatSpec with Matchers with ScalatestRouteTest with Routes
   it should "return the number plate in the provided sentence" in {
     Get("/api/extractor?sentence=I%20use%20to%20own%20a%20KAB%20892F") ~> routes ~> check {
       status should be (OK)
-      responseAs[String] should equal ("KAB 892F")
+      responseAs[String] should equal ("{\"result\":\"KAB 892F\"}")
     }
   }
 
   it should "fail for the extractor route if 'sentence' query parameter is missing" in {
     Get("/api/extractor") ~> routes ~> check {
-      status should not be (OK)
-      responseAs[String] should equal ("Request is missing required query parameter 'sentence'")
+      rejection should be (MissingQueryParamRejection("sentence"))
     }
 
+  }
+
+  it should "return 'NO PLATE' if its an empty request" in {
+    Get("/api/extractor?sentence=") ~> routes ~> check {
+      status should be (OK)
+      responseAs[String] should equal ("{\"result\":\"NO PLATE\"}")
+    }
+  }
+
+  it should "return the correct count for two valid plates" in {
+    Get("/api/difference?firstPlate=KBA%20001A&secondPlate=KBA%20002A") ~> routes ~> check {
+      status should be (OK)
+      responseAs[String] should be ("{\"result\":1}")
+    }
+  }
+
+  it should "return 0 if one of the plates is invalid" in {
+    Get("/api/difference?firstPlate=KB1%20001A&secondPlate=KBA%20002A") ~> routes ~> check {
+      status should be (OK)
+      responseAs[String] should be ("{\"result\":0}")
+    }
   }
 
 }
